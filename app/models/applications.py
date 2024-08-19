21b7d11da4b3302d946bda253c9171cf2a337f29
Lonely_Colonel <lonely_colonel@mail.ru>
2024-08-20 01:54:55 +0300

    cosmetic change

--- app/models/applications.py
+++ app/models/applications.py
@@ -11,6 +11,5 @@ class ApplicationsBase(SQLModel):
 class Application(ApplicationsBase, table=True):
     project_id: int = Field(foreign_key='project.id')
     project: 'Project' = Relationship(back_populates='applications')
-    # candidate_id: 'Profile' = Relationship(back_populates='profile.user_id')
     candidate_id: List['Profile'] = Relationship(back_populates='profile.user_id')
 
