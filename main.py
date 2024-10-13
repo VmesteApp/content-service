4d0a0d605f20c2b6f756a1ba76759471a252b830
ZaharPavlikov <94977317+ZaharPavlikov@users.noreply.github.com>
2024-10-13 16:15:02 +0300

    add CorsMiddleware

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import os
 from fastapi import FastAPI
 from app.api.routers import tag_router, pulse_router, application_router
 from app.api.jwt_decoder import AuthenticationMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(openapi_prefix="/content")
 
 
 app.add_middleware(AuthenticationMiddleware)
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = ["*"],
+  allow_methods = ["*"],
+  allow_headers = ["*"]
+)
+
+
 app.include_router(application_router.router)
 app.include_router(pulse_router.router)
 app.include_router(tag_router.router)
