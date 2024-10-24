182af8b392fc1e898330807e10771ea1bc53150f
Lonely_Colonel <lonely_colonel@mail.ru>
2024-10-25 02:39:28 +0300

    fix middleware

--- app/api/jwt_decoder.py
+++ app/api/jwt_decoder.py
@@ -1,3 +1,4 @@
+import re
 import jwt
 from fastapi import Request
 from fastapi.responses import JSONResponse
@@ -6,12 +7,16 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+EXEMPT_PATHS = [r"/content/image/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}"]
 
 
 class AuthenticationMiddleware(BaseHTTPMiddleware):
 
     async def dispatch(self, request: Request, call_next):
         if request.url.path.startswith(("/content/docs", "/content/redoc", "/content/openapi.json")):
+            return await call_next(request)
+
+        if self.is_exempt_path(request.url.path):
             return await call_next(request)
 
         token = request.headers.get("authorization")
@@ -22,8 +27,7 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
             request.state.role = self.decode_token(token)["role"]
             request.state.uid = self.decode_token(token)["uid"]
             jwt.decode(token, "test", algorithms=["HS256"])
-            response = await call_next(request)
-            return response
+            return await call_next(request)
 
         except jwt.ExpiredSignatureError:
             return JSONResponse(status_code=401, content={"detail": "Token has expired"})
@@ -40,3 +44,9 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
     def decode_token(self, token: str) -> bool:
         payload = jwt.decode(token, "test", algorithms=["HS256"])
         return payload
+
+    def is_exempt_path(self, path: str):
+        for exempt_path in EXEMPT_PATHS:
+            if re.fullmatch(exempt_path, path):
+                return True
+        return False
