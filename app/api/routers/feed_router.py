42515951d32c030d97d90bdb078ba5254bfe3b75
Lonely_Colonel <lonely_colonel@mail.ru>
2024-10-27 23:50:14 +0300

    fix feed again

--- app/api/routers/feed_router.py
+++ app/api/routers/feed_router.py
@@ -1,17 +1,24 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from app.db.session import get_db
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
-from app.models.models import pulse, images, pulse_tags, tag
+from app.models.models import pulse, images, pulse_tags, tag, pulse_members
 
 
 router = APIRouter()
 
 
 @router.get("/feed")
-def get_feed(session: Session = Depends(get_db)):
-    result = session.query(pulse).all()
+def get_feed(request: Request, session: Session = Depends(get_db)):
+    query = (
+        session.query(pulse)
+        .outerjoin(pulse_members, pulse.c.id == pulse_members.c.pulse_id)
+        .filter(and_(pulse.c.founder_id != request.state.uid, pulse_members.c.user_id != request.state.uid))
+        .distinct()
+    )
+    result = query.all()
     tags = session.query(pulse_tags.c.pulse_id, tag.c.name, tag.c.id).join(tag, tag.c.id == pulse_tags.c.tag_id).all()
-
+    
     return [{"id": res.id,
             "category": res.category,
             "name": res.name,
