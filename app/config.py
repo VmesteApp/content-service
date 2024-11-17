f53bce365dd982a41f4da15e4f6157c9d04ada0a
ZaharPavlikov <94977317+ZaharPavlikov@users.noreply.github.com>
2024-11-17 18:41:26 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,3 +9,4 @@ SECRET = os.environ.get("SECRET")
 MODEL_PATH = os.environ.get("MODEL_PATH")
 VOCAB_PATH = os.environ.get("VOCAB_PATH")
 MODERATION_ON = os.environ.get("MODERATION_ON") == "True"
+vk_token = os.environ.get("vk_token")
