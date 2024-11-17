34dd801e7198113b3b147c7486c884b8f09799ec
ZaharPavlikov <94977317+ZaharPavlikov@users.noreply.github.com>
2024-11-17 17:54:05 +0300

    add notofication router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from fastapi import FastAPI
-from app.api.routers import tag_router, pulse_router, application_router, upload_router, feed_router, complaints_router
+from app.api.routers import tag_router, pulse_router, application_router, upload_router, feed_router, complaints_router, notification_router
 from app.api.jwt_decoder import AuthenticationMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -22,4 +22,5 @@ app.include_router(tag_router.router)
 app.include_router(upload_router.router)
 app.include_router(feed_router.router)
 app.include_router(complaints_router.router)
+app.include_router(notification_router.router)
 app.debug = os.environ.get("DEBUG")
